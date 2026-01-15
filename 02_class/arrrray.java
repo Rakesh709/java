4ce9c0fb4eee080624e2557f321ed49da5433697
Rakesh709 <kumar.rakesh1997@outlook.com>
2026-01-15 12:58:24 +0530

    multi dim array

--- 02_class/arrrray.java
+++ 02_class/arrrray.java
@@ -1,7 +1,26 @@
 public class arrrray {
 
      public static void main(String[] args){
-        int nums[] = {1,2,3,4};
-        System.out.println(nums[0]);
+      // 1D Array
+      //   int nums[] = {1,2,3,4};
+      //   System.out.println(nums[0]);
+
+      //2D Array
+         int nums [] [] = new int [3][4];
+         
+         for(int i=0; i<3;i++){
+            for(int j=0;j<4;j++){
+               nums[i][j] = (int )(Math.random()*100);
+            }
+            
+         }
+
+         for(int i=0; i<3;i++){
+            for(int j=0;j<4;j++){
+               System.out.print(nums[i][j]+" ");
+            }
+            System.out.println();
+         }
+
      }
 }
