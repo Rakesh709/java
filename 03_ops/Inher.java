21c38182e85ea4f4a0c49be2fc5ea27140c64f75
Rakesh709 <kumar.rakesh1997@outlook.com>
2026-01-16 22:59:32 +0530

    Inheritance

---
+++ 03_ops/Inher.java
@@ -0,0 +1,10 @@
+
+
+
+public class Inher {
+    public static void main(String[] a){
+        AdvCal obj = new AdvCal();
+        int r1= obj.mul(1, 2);
+        System.out.println(r1);
+    }    
+}
