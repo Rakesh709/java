5693c617683261eaebc863fa6ddf8c145a3f5ea7
Rakesh709 <kumar.rakesh1997@outlook.com>
2026-01-26 11:54:45 +0530

    Abstract in java

---
+++ 05_Adav_Java/AbstractJava.java
@@ -0,0 +1,19 @@
+
+class Car{
+    public void drive(){
+
+    }
+
+    public void playMusic(){
+        System.out.println("Music Playing.");
+    }
+}
+
+
+
+public class AbstractJava {
+    public static void main(String[] args) {
+        Car bmw = new Car();
+        
+    }    
+}
