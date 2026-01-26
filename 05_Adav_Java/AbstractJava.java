034a7bfa5f22c2eec426fb111d1ca5fa54e60a40
Rakesh709 <kumar.rakesh1997@outlook.com>
2026-01-26 12:09:48 +0530

    abastract class

--- 05_Adav_Java/AbstractJava.java
+++ 05_Adav_Java/AbstractJava.java
@@ -1,19 +1,39 @@
 
-class Car{
-    public void drive(){
+abstract class Car{
+    public abstract void drive();
+    public abstract void fly();
 
-    }
+    //abstract method belong to only abstarct class
+    //important 
+    //abastract class can be without abasract method
+    //once you created abasrract method then call it in extend class
 
     public void playMusic(){
         System.out.println("Music Playing.");
     }
 }
 
+abstract class WagnaR extends Car{
+    public void drive(){
+        System.out.println("Driving wagnaR");
+    }
+
+    // public void fly(){
+    //     System.out.println("flying.");
+    // }
+    //not able implemented method then that class will become abastarct class
+}
+
+//concrete class
+class UpdatedWagnarR  extends WagnaR{
+    public void fly() {
+        throw new UnsupportedOperationException("Unimplemented method 'fly'");
+    }}
 
 
 public class AbstractJava {
     public static void main(String[] args) {
-        Car bmw = new Car();
-        
+        Car car = new UpdatedWagnarR();
+        car.fly();
     }    
 }
