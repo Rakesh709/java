5924aeccff78c519c01c862ec1352b0d575e7d14
Rakesh709 <kumar.rakesh1997@outlook.com>
2026-01-28 00:18:25 +0530

    functional interface

--- 05_Adav_Java/AdvanceInterface.java
+++ 05_Adav_Java/AdvanceInterface.java
@@ -4,23 +4,39 @@
 //3. Marker : no methods
 
 
+// lambda funtion works only with Funtinal interface
+
+
 @FunctionalInterface
 interface A{
-    void show();
+    void show(int i);
     //void read(); Invalid '@FunctionalInterface' annotation; A is not a functional
 }
 
-class B implements A{
-    public void show(){
-        System.out.println("In show");
-    }
+@FunctionalInterface
+interface B{
+    int sum(int i,int j);
 }
+
+// class B implements A{
+//     public void show(){
+//         System.out.println("In show");
+//     }
+// }
 
 
 
 
 public class AdvanceInterface {
     public static void main(String[] args) {
-        
+        A obj = (int i) -> System.out.println("In Show"+ i);
+        obj.show(5);
+
+        B s = (i,j) -> i+j;
+
+        int result = s.sum(1,2);
+        System.out.println(result);
     }
 }
+
+// -> : this is lambda expression : 
