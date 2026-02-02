c4421e694480afd440b00b766101c0d26bf1ac53
Rakesh709 <kumar.rakesh1997@outlook.com>
2026-02-02 20:19:42 +0530

    jdbc

--- 08_maven/JDBC/src/DemoJdbc.java
+++ 08_maven/JDBC/src/DemoJdbc.java
@@ -19,12 +19,26 @@ public class DemoJdbc {
         String url = "jdbc:postgresql://localhost:5432/demo";
         String uname = "postgres";
         String pass = "1111";
+        String sql = "select sname from student where sid = 1";
 
         Class.forName("org.postgresql.Driver");
 
         Connection con = DriverManager.getConnection(url,uname,pass);
 
         System.out.println("Connection DONE");
+
+        Statement st = con.createStatement();
+        ResultSet  rs =st.executeQuery(sql);
+
+        rs.next();
+
+        String name = rs.getString("sname");
+
+        System.out.println(rs.next());
+        con.close();
+        System.out.println("Connection Close");
+
+
 
     }
 }
