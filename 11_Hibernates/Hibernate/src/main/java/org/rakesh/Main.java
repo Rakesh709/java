894f35cbb8f3f655db51e11a4bcaef33334acace
Rakesh709 <kumar.rakesh1997@outlook.com>
2026-02-05 00:26:43 +0530

    fetch Hibernate query

--- 11_Hibernates/Hibernate/src/main/java/org/rakesh/Main.java
+++ 11_Hibernates/Hibernate/src/main/java/org/rakesh/Main.java
@@ -16,18 +16,29 @@ public class Main {
         s1.setRollNo(103);
         s1.setsName("Galaxy");
         s1.setAge(33);
+
+        Student s2= null;
+
         Configuration cfg = new Configuration();
         cfg.addAnnotatedClass(Student.class);
         cfg.configure("hibernate.cfg.xml");
+
+
         SessionFactory sf = cfg.buildSessionFactory();
         Session session = sf.openSession();
-        Transaction transaction = session.beginTransaction();
 
-        session.persist(s1);
+        //Transaction transaction = session.beginTransaction();
+        //no need transaction need when do some update and all
 
-        transaction.commit();
+        //session.persist(s1);
+
+        s2 = session.find(Student.class, 101);
+
+
+        //transaction.commit();
         session.close();
         sf.close();
+        //System.out.println(s1);
         System.out.println(s1);
 
 
