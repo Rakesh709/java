404102c6d682e4e29e48e89379c0241f32ae294f
Rakesh709 <kumar.rakesh1997@outlook.com>
2026-02-06 11:52:34 +0530

    scopes in spring

--- 13_Sprint_start/Spring1/src/main/java/org/example/Alien.java
+++ 13_Sprint_start/Spring1/src/main/java/org/example/Alien.java
@@ -2,6 +2,12 @@ package org.example;
 
 public class Alien {
 
+    int age;
+    //constructor
+    public Alien(){
+        System.out.println("Object created");
+    }
+
     public  void code(){
         System.out.println("coding...");
     }
--- 13_Sprint_start/Spring1/src/main/java/org/example/Main.java
+++ 13_Sprint_start/Spring1/src/main/java/org/example/Main.java
@@ -8,10 +8,26 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) {
 
-        //below one will create a container in spring.xml
+        //below one will create a container in spring.xml -> create a object as well
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-        Alien obj = (Alien) context.getBean("alien");
-        obj.code();
+        Alien obj1 = (Alien) context.getBean("alien");
+        obj1.age=24;
+        System.out.println(obj1.age);
+        //obj1.code();
+
+        Alien obj2 = (Alien) context.getBean("alien");
+        obj2.age=25;
+        System.out.println(obj2.age);
+        //obj2.code();
 
     }
 }
+
+//scope in beans
+//1. singleton
+//2. prototype
+
+// if you created object multiple time always it will call to one bean only
+
+// singleton: create a object only one time
+//prototype : it will create object multiple time
