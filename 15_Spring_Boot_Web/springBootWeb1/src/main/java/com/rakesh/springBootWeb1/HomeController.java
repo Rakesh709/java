042a9695c80dc4bf3c377892f91d45ee71f8eb9d
Rakesh709 <kumar.rakesh1997@outlook.com>
2026-02-14 23:15:56 +0530

    JSP mvc springboot

--- 15_Spring_Boot_Web/springBootWeb1/src/main/java/com/rakesh/springBootWeb1/HomeController.java
+++ 15_Spring_Boot_Web/springBootWeb1/src/main/java/com/rakesh/springBootWeb1/HomeController.java
@@ -1,5 +1,8 @@
 package com.rakesh.springBootWeb1;
 
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,6 +12,20 @@ public class HomeController {
     @RequestMapping("/")
     public String home(){
         return "index.jsp";
+    }
+
+    @RequestMapping("add")
+    public String add(HttpServletRequest req, HttpSession session){
+        System.out.println("is reach add");
+
+        int num1 = Integer.parseInt(req.getParameter("num1"));
+        int num2 = Integer.parseInt(req.getParameter("num2"));
+
+        int result = num1+num2;
+        session.setAttribute("result",result);
+        System.out.println(result);
+
+        return "result.jsp";
     }
 
 }
--- 15_Spring_Boot_Web/springBootWeb1/src/main/webapp/index.jsp
+++ 15_Spring_Boot_Web/springBootWeb1/src/main/webapp/index.jsp
@@ -1,13 +1,19 @@
-<%@ page language="java"%>
-
+<%@page language="java" %>
 
 <html>
-<head>
-    <meta charset="UTF-8">
-    <title>Sample Page Title</title>
-</head>
-<body>
-    <h1>This is a Heading</h1>
-    <p>This is a paragraph with some content.</p>
-</body>
+    <head>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    </body>
+        <h2>Telusko Calculator</h2>
+
+         <form action="add">
+                <label for="num1">Enter 1st Number :</label>
+                <input type="text" id=num1" name="num1"><br>
+                <label for="num2">Enter 2nd Number :</label>
+                <input type="text" id="num2" name="num2"><br>
+                <input type="submit" value="Submit">
+            </form>
+
+    </body>
 </html>
