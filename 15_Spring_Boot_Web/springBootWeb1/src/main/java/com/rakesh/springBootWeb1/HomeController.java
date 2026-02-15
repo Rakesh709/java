19395a61b0a7f386d409b4868ff2e7906c543a79
Rakesh709 <kumar.rakesh1997@outlook.com>
2026-02-15 13:26:14 +0530

    prefix and suffix springboot

--- 15_Spring_Boot_Web/springBootWeb1/src/main/java/com/rakesh/springBootWeb1/HomeController.java
+++ 15_Spring_Boot_Web/springBootWeb1/src/main/java/com/rakesh/springBootWeb1/HomeController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -12,22 +13,23 @@ public class HomeController {
 
     @RequestMapping("/")
     public String home(){
-        return "index.jsp";
+        return "index";
     }
 
     @RequestMapping("add")
 //    public String add(HttpServletRequest req, HttpSession session){
-        public String add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, HttpSession session){
+        public String add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, Model model){
         System.out.println("is reach add");
 
 //        int num1 = Integer.parseInt(req.getParameter("num1"));
 //        int num2 = Integer.parseInt(req.getParameter("num2"));
 
         int result = num1+num2;
-        session.setAttribute("result",result);
+//        session.setAttribute("result",result);
+        model.addAttribute("result",result);
         System.out.println(result);
 
-        return "result.jsp";
+        return "result";
     }
 
 }
