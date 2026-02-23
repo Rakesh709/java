5563e01790c279959f25f1d444431c10401710ea
Rakesh709 <kumar.rakesh1997@outlook.com>
2026-02-23 23:36:54 +0530

    started with rest api

--- 16_RestAPI/RestApiDev/src/main/java/com/rakesh/RestApiDev/controller/StudentController.java
+++ 16_RestAPI/RestApiDev/src/main/java/com/rakesh/RestApiDev/controller/StudentController.java
@@ -2,16 +2,30 @@ package com.rakesh.RestApiDev.controller;
 
 
 import com.rakesh.RestApiDev.dto.StudentDto;
+import com.rakesh.RestApiDev.repository.StudentRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentController {
 
-    @GetMapping("/student")
-    public StudentDto getStudent(){
-        return new StudentDto(4L,"Rakesh","rakesh@gmail.com");
+    private final StudentRepository studentRepository;
+
+//    @GetMapping("/student")
+//    public StudentDto getStudent(){
+//        return new StudentDto(4L,"Rakesh","rakesh@gmail.com");
+//    }
+
+
+    public StudentController(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
     }
+
+    @GetMapping("/student")
+    public List<Student> getStudent(){
+        return studentRepository.findAll();
+    }
+
 
     @GetMapping("/student/{id}")
     public StudentDto getStudentById(){
