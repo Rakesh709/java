c04e6fdedf6948c0283c42b8dd5568fcf3cfaa64
Rakesh709 <kumar.rakesh1997@outlook.com>
2026-03-13 23:41:02 +0530

    set

--- 05_Adav_Java/ArrayListInJava.java
+++ 05_Adav_Java/ArrayListInJava.java
@@ -1,13 +1,19 @@
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class ArrayListInJava {
 
     public static void main(String[] args) {
-        Collection<Integer> nums = new ArrayList<Integer>();
+        // Collection<Integer> nums = new ArrayList<Integer>();
+        // List<Integer> nums = new ArrayList<Integer>();
+        Set<Integer> nums = new HashSet<Integer>();
+
         nums.add(6);
-        nums.add(6);
-        nums.add(6);
+        nums.add(5);
+        nums.add(4);
 
         for(int n: nums){
              System.out.println(n);
